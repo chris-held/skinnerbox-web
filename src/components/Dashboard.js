6f6fddbbaa4f62559b7d7baf3aad5c8be0a90c9c
Chris Held <cheld100@gmail.com>
2019-07-16 06:55:50 -0500

    convert to apollo v3, update dashboard query to use hooks

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { Container, Grid } from '@material-ui/core';
-import { Query } from 'react-apollo';
+import { useQuery } from 'react-apollo';
 import gql from 'graphql-tag';
 import Header from './Header';
 import { EnforceAuth } from './EnforceAuth';
@@ -15,48 +15,48 @@ const ME = gql`
   }
 `;
 
-const Dashboard = () => (
-  <EnforceAuth>
-    {/* TODO: get relevant data from apollo */}
-    <Query query={ME}>
-      {({ loading, error, data: { me } = {} }) => {
-        if (loading) {
-          return <div>Loading...</div>;
-        }
-        if (error) {
-          return <div>Error!</div>;
-        }
-        console.log(me);
-        return (
-          <Fragment>
-            <Header title="0" subtitle="Current Balance" />
-            <Container>
-              <Grid container item xs={12} spacing={3}>
-                <Grid item xs={6}>
-                  <ActivityList
-                    title="Tasks"
-                    activities={tasks}
-                    onCreate={() => {}}
-                    onActivityComplete={() => {}}
-                    onActivityEdit={() => {}}
-                  />
-                </Grid>
-                <Grid item xs={6}>
-                  <ActivityList
-                    title="Rewards"
-                    activities={rewards}
-                    onCreate={() => {}}
-                    onActivityComplete={() => {}}
-                    onActivityEdit={() => {}}
-                  />
-                </Grid>
-              </Grid>
-            </Container>
-          </Fragment>
-        );
-      }}
-    </Query>
-  </EnforceAuth>
-);
+const Loading = () => <div>Loading...</div>;
+
+const Error = () => <div>Error!</div>;
+
+const Dashboard = () => {
+  const { loading, error, data: { me } = {} } = useQuery(ME);
+  if (loading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <Error />;
+  }
+  console.log(me);
+  return (
+    <EnforceAuth>
+      <Fragment>
+        <Header title="0" subtitle="Current Balance" />
+        <Container>
+          <Grid container item xs={12} spacing={3}>
+            <Grid item xs={6}>
+              <ActivityList
+                title="Tasks"
+                activities={tasks}
+                onCreate={() => {}}
+                onActivityComplete={() => {}}
+                onActivityEdit={() => {}}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <ActivityList
+                title="Rewards"
+                activities={rewards}
+                onCreate={() => {}}
+                onActivityComplete={() => {}}
+                onActivityEdit={() => {}}
+              />
+            </Grid>
+          </Grid>
+        </Container>
+      </Fragment>
+    </EnforceAuth>
+  );
+};
 
 export default Dashboard;
